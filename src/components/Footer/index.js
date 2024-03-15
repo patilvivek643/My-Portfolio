1f3daec600f8c601ad5b39803d44d1d1eb60c6cc
patilvivek643 <om6431019@1210>
2024-03-15 21:25:36 +0530

    resume link change and instagram logo from bottom

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -100,10 +100,10 @@ function Footer() {
           {/* <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
           <SocialMediaIcon href={Bio.twitter} target="display"><TwitterIcon /></SocialMediaIcon> */}
           <SocialMediaIcon href={Bio.linkedin} target="display"><LinkedInIcon /></SocialMediaIcon>
-          <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
+          {/* <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon> */}
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Vivek Patil. All rights reserved.
+          &copy; 2024 Vivek Patil. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
